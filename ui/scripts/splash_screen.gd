83600a512084c8ffd67763aca3f1fee1ab5dd3da
Blocker226 <neotwt@gmail.com>
2025-08-04 02:26:57 +0800

    Post-jam tweaks to plants

--- ui/scripts/splash_screen.gd
+++ ui/scripts/splash_screen.gd
@@ -1,9 +1,14 @@
 class_name SplashScreen extends Button
 
 @export var fade_duration: float = 1
+@export var start_visible: bool = true
 
 var _tween: Tween
 var _pressed: bool = false
+
+
+func _ready() -> void:
+	visible = start_visible
 
 
 func _on_pressed() -> void:
